76f143ee8aa8eb1d94e35e1d4d89527edc9a5ce0
Oskar <blindjoedeath@gmail.com>
2018-02-02 17:59:51 +0300

    Inited project

---
+++ Assets/Scripts/AudioClips.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+
+[Serializable]
+public class AudioClips : ScriptableObject {
+
+    [Serializable]
+    public class Clip
+    {
+        public AudioClip audioClip;
+        [Range(0, 1f)]
+        public float volume;
+    }
+
+    [MenuItem("Assets/Create/AudioClips")]
+    public static void CreateAsset()
+    {
+        ScriptableObjectUtility.CreateAsset<AudioClips>();
+    }
+   
+    [SerializeField]
+    public List<Clip> audioClips = new List<Clip>();
+
+    private int lastClip;
+
+    public void ResetLastClipIndex()
+    {
+        lastClip = 0;
+    }
+
+    public Clip NextClip()
+    {
+        if (lastClip == audioClips.Count)
+        {
+            lastClip = 0;
+        }
+        return audioClips[lastClip++];
+    }
+
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private float xMouse;
+    private float yMouse;
+
+    private int speedHash;
+    private int shiftHash;
+    private int screamHash;
+
+    private int idleStateSash;
+
+    private Animator animator;
+    private AudioSource audioSource;
+    public AudioClips audioClips;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+        audioClips.ResetLastClipIndex();
+
+        speedHash = Animator.StringToHash("Speed");
+        shiftHash = Animator.StringToHash("Shift");
+        screamHash = Animator.StringToHash("Scream");
+
+        idleStateSash = Animator.StringToHash("Base Layer.Idle");
+    }
+
+    private void Update()
+    {
+        float vertAxis = Input.GetAxis("Vertical");
+        float horAxis = Input.GetAxis("Horizontal");
+        AnimatorStateInfo animStateInfo = animator.GetCurrentAnimatorStateInfo(0); 
+
+
+
+        animator.SetFloat(speedHash, vertAxis);
+        animator.SetBool(shiftHash, Input.GetKey(KeyCode.LeftShift));
+        if (Input.GetKeyDown(KeyCode.E) && animStateInfo.fullPathHash == idleStateSash)
+        {
+            PlayClip();
+            animator.SetTrigger(screamHash);
+            audioSource.Play();
+        }
+
+    }
+
+    private void PlayClip()
+    {
+        var clip = audioClips.NextClip();
+        audioSource.volume = clip.volume;
+        audioSource.clip = clip.audioClip;
+
+        audioSource.Play();
+    }
+}
+
