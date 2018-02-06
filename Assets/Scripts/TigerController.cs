af9446386f04f18d7f2ad74edf7333ca39e4bd02
Oskar <blindjoedeath@gmail.com>
2018-02-06 18:52:50 +0300

    Modified scene

--- Assets/Scripts/TigerController.cs
+++ Assets/Scripts/TigerController.cs
@@ -30,12 +30,15 @@ public class TigerController : MonoBehaviour {
     private AudioSource audioSource;
     private Rigidbody rigidBody;
     private Camera playerCamera;
-    private Collider terrainCollider;
     private Collider collider;
     private ParticlesController particlesController;
+    private Transform environmentBase;
+
     AnimatorStateInfo animStateInfo;
 
     private float groundDist;
+    private bool isGrounded;
+    private Vector3[] rayOrigins = new Vector3[4];
 
     public AudioClips audioClips;
 
@@ -45,11 +48,20 @@ public class TigerController : MonoBehaviour {
         audioSource = GetComponent<AudioSource>();
         rigidBody = GetComponent<Rigidbody>();
         playerCamera = GetComponentInChildren<Camera>();
-        terrainCollider = FindObjectOfType<Terrain>().GetComponent<Collider>();
         collider = GetComponent<Collider>();
         particlesController = GetComponentInChildren<ParticlesController>();
+        environmentBase = GameObject.FindGameObjectWithTag("Environment").transform;
+
 
         groundDist = collider.bounds.extents.y;
+        var center = collider.bounds.center;
+
+        rayOrigins[0] = new Vector3(center.x - collider.bounds.extents.x, center.y, center.z - collider.bounds.extents.z);
+        rayOrigins[1] = new Vector3(center.x - collider.bounds.extents.x, center.y, center.z + collider.bounds.extents.z);
+        rayOrigins[2] = new Vector3(center.x + collider.bounds.extents.x, center.y, center.z - collider.bounds.extents.z);
+        rayOrigins[3] = new Vector3(center.x + collider.bounds.extents.x, center.y, center.z + collider.bounds.extents.z);
+
+
         audioClips.ResetLastClipIndex();
 
         foreach(var param in parameters)
@@ -63,14 +75,14 @@ public class TigerController : MonoBehaviour {
         }
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         float vertAxis = Input.GetAxis("Vertical");
         float horAxis = Input.GetAxis("Horizontal");
         float mouseHorAxis = Input.GetAxis("Mouse X");
         float mouseVertAxis = Input.GetAxis("Mouse Y");
 
-
+        Debug.Log(isGrounded);
         playerCamera.transform.RotateAround(transform.position, Vector3.up, mouseHorAxis);
 
         transform.Rotate(new Vector3(0, horAxis, 0));
@@ -96,14 +108,13 @@ public class TigerController : MonoBehaviour {
                 animator.SetTrigger(paramHash["Hit"]);
                 PlayParticles("Hit");
             }
-            else if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+            else if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
             {
                 animator.SetTrigger(paramHash["Jump"]);
                 rigidBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             }
         }
-
-        if (IsGrounded())
+        if (isGrounded)
         {
             Vector3 delta = transform.forward * vertAxis;
             if (animStateInfo.fullPathHash == stateHashes["Walk"])
@@ -118,6 +129,34 @@ public class TigerController : MonoBehaviour {
         }
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        CheckGround();
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        CheckGround();
+    }
+
+    private void CheckGround()
+    {
+        RaycastHit hit = new RaycastHit();
+        foreach (var point in rayOrigins)
+        {
+            Physics.Raycast(new Ray(point, -transform.up), out hit, groundDist + 0.1f);
+            if (hit.collider != null)
+            {
+                if (!hit.collider.transform.IsChildOf(environmentBase))
+                {
+                    isGrounded = false;
+                    return;
+                }
+            }
+        }
+        isGrounded = true;
+    }
+
     private void PlayClip()
     {
         var clip = audioClips.NextClip();
@@ -125,13 +164,6 @@ public class TigerController : MonoBehaviour {
         audioSource.clip = clip.audioClip;
 
         audioSource.Play();
-    }
-
-    private bool IsGrounded()
-    {
-        RaycastHit hit = new RaycastHit();
-        Physics.Raycast(new Ray(collider.bounds.center, -transform.up), out hit, groundDist + 0.2f);
-        return hit.collider == terrainCollider;
     }
 
     private void PlayParticles(string state)
